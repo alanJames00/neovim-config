480e81ae1525527b341a39f00880fdf73f3e65bb
alanjames00 <alanjames6282@gmail.com>
2024-06-18 12:01:03 +0530

    sync packages and mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,28 +2,39 @@ local M = {}
 
 -- stores all the dap based keymappings
 M.dap = {
-  plugin = true,
-  n = {
-    ["<leader>db"] = {
-      "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line"
-    },
-    ["<leader>dr"] = {
-      "<cmd> DapContinue <CR>",
-      "Run or continue the debugger"
-    }
-  }
+	plugin = true,
+	n = {
+		["<leader>db"] = {
+			"<cmd> DapToggleBreakpoint <CR>",
+			"Add breakpoint at line"
+		},
+		["<leader>dr"] = {
+			"<cmd> DapContinue <CR>",
+			"Run or continue the debugger"
+		}
+	}
 }
 
 -- stores all the custom keymappings
 M.custom = {
-  plugin = false,
-  n = {
-    ["<leader>tt"] = {
-      "<cmd> terminal <CR>",
-      "Open Terminal in a new Tab"
-    }
-  }
+	plugin = false,
+	n = {
+		["<leader>tt"] = {
+			"<cmd> terminal <CR>",
+			"Open Terminal in a new Tab"
+		}
+	}
+}
+
+-- stores all the custom keymappings for live-server.nvim
+M.custom = {
+	plugin = true,
+	n = {
+		["<leader>ls"] = {
+			"<cmd> LiveServerToggle <CR>",
+			"Toggle live-server on and off"
+		}
+	}
 }
 
 return M
